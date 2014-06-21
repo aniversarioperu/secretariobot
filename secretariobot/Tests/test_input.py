840023646e5f7c73dbcd35d91c73af4d8c7023c3
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-22 02:00:09 +0300

    tested get status

--- secretariobot/Tests/test_input.py
+++ secretariobot/Tests/test_input.py
@@ -7,14 +7,21 @@ from secretariobot.Input import Input
 
 class InputTest(unittest.TestCase):
 
+    def setUp(self):
+        self.tweet_id = "480428719463337985"
+
     def test_clean(self):
-        tweet_id = "480428719463337985"
-        obj = Input(tweet_id)
+        obj = Input(self.tweet_id)
 
         expected_string = u'y el uterope ya aparece en la lista de usuarios de Scrapy ver final de página'
         result = obj.clean_string
-
         self.assertEqual(result, expected_string.encode("utf-8"))
+
+    def test_get_status(self):
+        expected = u"y el @uterope ya aparece en la lista de usuarios de Scrapy http://t.co/qZlUr7yRRO (ver final de página)"
+        obj = Input(self.tweet_id)
+        result = obj.input_string
+        self.assertEqual(result, expected)
 
     def test_is_reply(self):
         tweet_id = "479726791259860992"
