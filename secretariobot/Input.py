7b090b489cc01aa2166e2031ca771a35f7d9232b
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-21 20:27:09 +0300

    first commit

---
+++ secretariobot/Input.py
@@ -0,0 +1,7 @@
+class Input:
+    def __init__(self, input_string):
+        self.clean_input = self.clean(input_string)
+
+    def clean(input_strig):
+        return input_string
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='secretariobot',
+    version='0.0.1',
+    url="https://github.com/aniversarioperu/secretariobot",
+    author="AniversarioPeru",
+    author_email="aniversarioperu1@gmail.com",
+    maintainer="AniversarioPeru",
+    maintainer_email="aniversarioperu1@gmail.com",
+    contact="AniversarioPeru",
+    contact_email="aniversarioperu1@gmail.com",
+    license="GPL v3",
+    description="be a secretariobot",
+    long_description=open('README.md').read(),
+    platforms="any",
+    download_url="",
+    classifiers=[
+        "Programming Language :: Python",
+        ("Topic :: Scientific/Engineering :: Bio-Informatics"),
+        ("Intended Audience :: Science/Research"),
+        ("License :: OSI Approved :: GNU General Public License v3 (GPLv3)"),
+        ("Operating System :: OS Independent"),
+        ("Environment :: Console"),
+    ],
+    install_requires=[],
+    packages=['secretariobot'],
+)
+
